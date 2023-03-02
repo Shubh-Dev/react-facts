5f94ae0299c7d00044fe6ff07ca06d00837d64b1
Shubh <shubh.scb@gmail.com>
2023-03-02 13:53:57 +0530

    components add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
 }
-
-export default App;
